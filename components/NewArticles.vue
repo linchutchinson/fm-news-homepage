46dc194b4815a0ff3211c7b35bf13ff8651ec569
Lincoln Hutchinson <me@linchutchinson.com>
2023-02-11 23:12:41 -0800

    Add hover color to new articles

--- components/NewArticles.vue
+++ components/NewArticles.vue
@@ -3,9 +3,13 @@
     <h2 class="text-soft-orange text-3xl font-extrabold">New</h2>
     <div v-for="(article, index) in articles" class="grow">
       <hr v-if="index > 0" class="border-1 border-blue-dark-grayish mt-4" />
-      <h3 class="text-off-white font-bold mt-4 mb-2 lg:text-xl">
-        {{ article.title }}
-      </h3>
+      <NuxtLink to="#">
+        <h3
+          class="text-off-white font-bold mt-4 mb-2 lg:text-xl hover:text-soft-orange transition duration-100 ease-in-out"
+        >
+          {{ article.title }}
+        </h3>
+      </NuxtLink>
       <p class="text-blue-grayish lg:text-lg">{{ article.snippet }}</p>
     </div>
   </div>
