bda93dca87783eced24d7d9a886a2ee48ac2c135
Lincoln Hutchinson <me@linchutchinson.com>
2023-02-13 17:16:32 -0800

    Config for seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,13 @@ export default defineNuxtConfig({
       viewport: "width=device-width, initial-scale=1.0",
       title: "Frontend Mentor | News Homepage",
       link: [{ rel: "icon", href: "/favicon.png", sizes: "32x32" }],
+      meta: [
+        {
+          name: "description",
+          content: "A news frontpage with various articles on technology.",
+        },
+      ],
+      htmlAttrs: { lang: "en" },
     },
   },
   modules: ["@nuxtjs/tailwindcss"],
